c37cccaba05a8d4938cf600995d050d1d22cd573
koshi6345 <k0929s0313@gmail.com>
2021-08-09 05:12:03 +0000

    set-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users,         only:   [:show, :edit, :update]
+  resources :posts,         except: [:show] do
+    resources :comments,      only:   [:create, :destroy]
+    resource  :favorits,      only:   [:create, :destroy]
+  end
+  resources :relationships, only:   [:create, :destroy]
+  resources :tags,          only:   [:create]
+  resources :searchs,       only:   [:index]
+  resources :rooms,         only:   [:create, :show]
+  resources :messages,      only:   [:create]
 end
