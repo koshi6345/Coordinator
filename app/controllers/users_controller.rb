20376ec31f878ad870d91866648cc3a3f708513b
koshi6345 <k0929s0313@gmail.com>
2021-08-10 05:08:20 +0000

    user finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:image_id, :name, :introduction)
+  end
+
 end
