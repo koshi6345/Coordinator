1c89e78553f9178a19c621a2275321a6e06a537e
koshi6345 <k0929s0313@gmail.com>
2021-08-10 14:48:15 +0000

    post finish

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users,         only:   [:show, :edit, :update]
   root to: 'posts#index'
-  resources :posts,         except: [:show, :index] do
+  resources :posts,         except: [:index] do
     resources :comments,      only:   [:create, :destroy]
     resource  :favorits,      only:   [:create, :destroy]
   end
