dfe296711eed5915d6476e16764a2f4b0094486d
koshi6345 <k0929s0313@gmail.com>
2021-08-11 07:40:40 +0000

    comment finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   attachment :image
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
