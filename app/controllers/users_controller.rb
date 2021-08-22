e1d0299b68f0865910183222c1b3118457601d73
koshi6345 <k0929s0313@gmail.com>
2021-08-22 09:15:25 +0000

    error fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:image_id, :name, :introduction)
+    params.require(:user).permit(:image, :name, :introduction)
   end
 
 end
