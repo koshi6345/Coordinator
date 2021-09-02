b4947e1ef5dc461c7fe7f01c1b54286b50783d85
koshi6345 <k0929s0313@gmail.com>
2021-09-02 14:09:10 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_124723) do
+ActiveRecord::Schema.define(version: 2021_09_02_064141) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_29_124723) do
     t.string "comment", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "reply_id"
   end
 
   create_table "entries", force: :cascade do |t|
