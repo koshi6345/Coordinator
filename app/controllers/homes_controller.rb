b12fc959877ea7a0a80b6dc10c2b8cb7702bc673
koshi6345 <k0929s0313@gmail.com>
2021-09-21 05:37:23 +0000

    RANDOM swith code

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,8 @@
 class HomesController < ApplicationController
   def top
     # 開発環境なら"RANDOM()"、本番環境なら"RAND()"じゃなきゃエラー出る
-    @random_posts = Post.order("RANDOM()").limit(6)
+    switch_random_function = Rails.env.production? ? "RAND()" : "RANDOM()"
+    @random_posts = Post.order(switch_random_function).limit(6)
   end
 
   def about
