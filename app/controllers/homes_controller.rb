67f6c1dbba42167fd3706567f0bb4d4e57e49680
koshi6345 <k0929s0313@gmail.com>
2021-09-28 08:12:54 +0000

    responsive design

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def top
-    # 開発環境なら"RANDOM()"、本番環境なら"RAND()"じゃなきゃエラー出る
+    # 下の一行で本番環境か開発環境かを判別して記述を変更している
     switch_random_function = Rails.env.production? ? "RAND()" : "RANDOM()"
     @random_posts = Post.order(switch_random_function).limit(6)
   end
